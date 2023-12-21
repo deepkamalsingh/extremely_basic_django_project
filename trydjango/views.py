d9cb619c5347700e4d1bc69eb5fe41e54acfc7fb
deepkamalsingh <deepkamalsinghjassal@gmail.com>
2023-12-21 09:08:31 +0530

    completed till tutorial 17

---
+++ trydjango/views.py
@@ -0,0 +1,23 @@
+"""
+To render html web pages
+"""
+
+from django.http import HttpResponse
+from articles.models import Article
+from django.template.loader import render_to_string
+
+
+
+def home_view(request):
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    article = Article.objects.first()
+    context = {
+        "title": article.title,
+        "content": article.content,
+        "id": article.id
+    }
+    HTML_STRING = render_to_string('home-view.html', context=context)
+    return HttpResponse(HTML_STRING)
