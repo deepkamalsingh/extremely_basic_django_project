3c3ffd64bba35a23c23964fbd2de27caf6afd5ea
deepkamalsingh <deepkamalsinghjassal@gmail.com>
2023-12-24 17:16:18 +0530

    done till tutorial 23

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+def article_search_view(request):
+    # print("my name is ")
+    # print(request.GET)
+    query_dict = request.GET
+    query = query_dict.get("q")
+    try:
+        query = int(query)
+    except:
+        query = None 
+    obj = None 
+    if query:
+        obj = Article.objects.get(id=query)
+    context = {
+        "obj": obj
+    }
+    # print(dir(request))
+    return render(request, "articles/search.html", context=context)
+
+def article_detail_view(request, id):
+    article_obj = None
+    if id is not None:
+        article_obj = Article.objects.get(id=id)
+    context = {
+        "obj" : article_obj
+    }
+    print(article_obj)
+    return render(request,'articles/detail.html',context=context)
+
+# @csrf_exempt
+def article_create_view(request):
+    context = {}
+    if request.method == "POST": 
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        obj = Article.objects.create(title=title,content=content)
+        context["obj"] = obj 
+        context["created"] = True 
+    return render(request,'articles/create.html',context=context)
--- trydjango/views.py
+++ trydjango/views.py
@@ -13,11 +13,9 @@ def home_view(request):
     Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
-    article = Article.objects.first()
+    article_queryset = Article.objects.all()
     context = {
-        "title": article.title,
-        "content": article.content,
-        "id": article.id
+        "article_list": article_queryset,
     }
     HTML_STRING = render_to_string('home-view.html', context=context)
     return HttpResponse(HTML_STRING)
